6c4a379fc6d82ea69e1addeca28998fd1e350054
Daniel Rieck <danielcrieck@gmail.com>
2016-01-21 21:06:06 -0800

    Added source file with code for handling gravity forms to build.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -3,6 +3,7 @@ var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files'
 concat([
  './transfer-central-custom.js',
  '../WSU-UE---JS/jQuery.oue-custom.js',
+ '../WSU-UE---JS/jQuery.forms.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
