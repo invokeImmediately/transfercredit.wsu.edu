8ef48fb2f6178a6e990616d7f3fbc2fd421c7e82
Daniel Rieck <danielcrieck@gmail.com>
2016-01-28 14:04:50 -0800

    Writing code for form interactivity: built function for highlighting unfilled required fields and indicating that a field group is ready.

--- JS/transfer-central-custom.js
+++ JS/transfer-central-custom.js
@@ -3,6 +3,43 @@
  *********************************************************************************************************************/
 (function ($) {
 	$(document).ready(function () {
-        // Add code here
+        traverseInputs('.ugrf-general-reqd');        
 	});
+    
+    function traverseInputs (selector) {
+        if ($.type(selector) === "string") {
+            $(selector).each(function () {
+                var $this = $(this);
+                var $inputs = $this.find('input');
+                $inputs.each(function () {
+                    var $thisChild = $(this);
+                    $thisChild.blur(function () {
+                        var $thisParent, $parentsInputs;
+                        var inputReady = true;
+                        
+                        if ($thisChild.val() == "") {
+                            $thisChild.removeClass('value-entered');
+                        }
+                        else {
+                            $thisChild.addClass('value-entered');
+                        }
+                        
+                        $thisParent = $thisChild.parents(selector);
+                        $parentsInputs = $thisParent.find('input');
+                        $parentsInputs.each(function () {
+                            if ($(this).val() == "") {
+                                inputReady = false;
+                            }
+                        });
+                        if (inputReady) {
+                            $thisParent.addClass('inputs-ready');
+                        }
+                        else {
+                            $thisParent.removeClass('inputs-ready');
+                        }
+                    });
+                });
+            });
+        }
+    }
 })(jQuery);
