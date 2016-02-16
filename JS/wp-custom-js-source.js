85512f3e8544f91dfc96388c8a80c77acabf4eb3
Daniel Rieck <danielcrieck@gmail.com>
2016-02-15 16:37:25 -0800

    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -314,17 +314,46 @@
             });
             $(".gform_body").on("change", selector + " input[type='file']", function () {
                 var $thisInput = $(this);
-//                if($thisChild.attr('type') == 'file') {
-                if($thisInput.prop("files").length > 0) {
-                    $thisInput.addClass("gf-value-entered");
+                if($thisInput.prop("files") != null && $thisInput.prop("files").length > 0) {
+                    var valuePassed = true;
                     var $parentOfInput = $thisInput.parents(selector).first();
-                    var $parentNextSblngs = $parentOfInput.nextAll(selector).first();
-                    $parentNextSblngs.removeClass("gf-hidden");
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector);
+                    var $parentPrevSblngs = $parentOfInput.prevAll(selector);
+                    if($parentNextSblngs.length != 0 || $parentPrevSblngs.length != 0) {
+                        var originalFileName = $thisInput.prop("files").item(0).name;
+                        $parentPrevSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                        $parentNextSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                    }
+                    if(valuePassed) {                      
+                        $thisInput.addClass("gf-value-entered");
+                        $parentNextSblngs.first().removeClass("gf-hidden");
+                    }
+                    else
+                    {
+                        $thisInput.get(0).value = "";
+                    }
                 }
                 else {
                     $thisChild.removeClass("gf-value-entered");
                 }
-//                }
             });
         }
     }
