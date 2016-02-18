65dd73cf903ecb47342a7a070355b2bcf77e9091
Daniel Rieck <danielcrieck@gmail.com>
2016-02-18 10:48:08 -0800

    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -6,8 +6,82 @@
         traverseInputs('.ugrf-general-reqd');        
         if($("div.gform_body").length > 0) {
             setupInstitutionSelectors(".oue-gf-instn-locn", ".oue-gf-instn-name");
+            setupEnhancedUploadChain(".oue-gf-upload-chain-enhncd", ".oue-gf-upload-chain-extra");
         }
 	});
+
+    /******************************************************************************************\
+    | Setup a chain of file uploading inputs, wherein only the left-most input in the tree is  |
+    | visible. As the user uploads files in sequence, the next nearest neighbor is unveiled.   |
+    \******************************************************************************************/
+    function setupEnhancedUploadChain (selector, selectorExtra) {
+        if ($.type(selector) === "string") {
+            /* CHECK IF UPLOADS ALREADY EXIST:
+             *  It is possible to arrive at this point in execution after the user has submitted a
+             *  form containing errors that also already contains transcripts uploaded to input
+             *  fields that will be hidden by default. The following blocks of code resolve this
+             *  situation by showing such fields, as well as their nearest neighbors.
+             */
+            var $inputs = $(selector + " input[type='file']");
+            $inputs.each(function () {
+                var $thisInput = $(this);
+                var $nextDiv = $thisInput.nextAll("div[id]").first();
+                if($nextDiv.length > 0) {
+                    $thisInput.addClass("gf-value-entered");
+                    var $parentOfInput = $thisInput.parents(selector).first();
+                    $parentOfInput.removeClass("gf-hidden");
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector).first();
+                    $parentNextSblngs.removeClass("gf-hidden");
+                }
+            });
+            $(".gform_body").on("change", selector + " input[type='file']", function () {
+                var $thisInput = $(this);
+                if($thisInput.prop("files") != null && $thisInput.prop("files").length > 0) {
+                    var valuePassed = true;
+                    var $parentOfInput = $thisInput.parents(selector).first();
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector);
+                    var $parentPrevSblngs = $parentOfInput.prevAll(selector);
+                    if($parentNextSblngs.length != 0 || $parentPrevSblngs.length != 0) {
+                        var originalFileName = $thisInput.prop("files").item(0).name;
+                        $parentPrevSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                        $parentNextSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                    }
+                    if(valuePassed) {                      
+                        var $parentNextExtra = $parentOfInput.nextAll(selectorExtra).first();
+                        $thisInput.addClass("gf-value-entered gf_left_half");
+                        $parentNextSblngs.first().removeClass("gf-hidden");
+                        $parentNextExtra.removeClass("gf-hidden");
+                    }
+                    else
+                    {
+                        alert("A file with the same name has already been uploaded; please choose a different file.");
+                        $thisInput.get(0).value = "";
+                    }
+                }
+                else {
+                    $thisChild.removeClass("gf-value-entered");
+                }
+            });
+        }
+    }
 
     /******************************************************************************************\
     | Setup activator checkboxes that disappear once one is selected                           |
