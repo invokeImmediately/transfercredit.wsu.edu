2ffc77df03b34dad2df22e09ab215525b7fec3a0
Daniel Rieck <danielcrieck@gmail.com>
2016-11-14 08:39:14 -0800

    Updating build
    Rebuilt production files to incorporate recent changes to source code.

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -4204,7 +4204,7 @@ function isJQuery($obj) {
 		// initScrollingSidebars("...");
         initWelcomeMessage(
 			"#welcome-message",
-			"post-welcome-message",
+			"#post-welcome-message",
 			1000,
 			500,
 			500
