31a15f9911e84b6e8919215086944a25c8fab8c9
Daniel Rieck <danielcrieck@gmail.com>
2018-07-16 11:47:27 -0700

    Update build dependency loading sequence
    Update loading sequence to reflect changes in how repositories of dependencies are organized

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -4,7 +4,10 @@ concat([
  './transfer-central-custom.js',
  '../WSU-UE---JS/jQuery.oue-custom.js',
  '../WSU-UE---JS/jQuery.forms.js',
- '../WSU-UE---JS/jQuery.qTip.js',
+ '../../jquery.AreYouSure/jquery.are-you-sure.js',
+ '../WSU-UE---JS/jquery.are-you-sure.js',
+ '../../qTip2/dist/jquery.qtip.min.js',
+ '../WSU-UE---JS/jQuery.qTip.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
  });
